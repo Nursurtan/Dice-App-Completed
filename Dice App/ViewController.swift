f781628f10eaa3b72d57a9af9fc407f050ecddbc
Nursurtan <arapovnur23@gmail.com>
2021-07-19 16:11:17 +0700

    Initial Commit

---
+++ Dice App/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by нурсултан арапов on 6/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
+   
+
+    @IBAction func rollButtonPressed(_sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "Dice1"), #imageLiteral(resourceName: "Dice2"), #imageLiteral(resourceName: "Dice3"), #imageLiteral(resourceName: "Dice4"), #imageLiteral(resourceName: "Dice5"), #imageLiteral(resourceName: "Dice6")]
+        
+        diceImageViewOne.image = diceArray.randomElement()
+        
+        diceImageViewTwo.image = diceArray.randomElement()
+        
+    }
+    
+}
+
