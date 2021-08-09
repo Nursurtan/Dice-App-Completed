ced0f9352dcaf0ae74f5180235458db881e7dec0
Nursurtan <arapovnur23@gmail.com>
2021-08-09 22:46:51 +0700

    Added a sound

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var audioPlayer:AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     @IBOutlet weak var diceImageViewOne: UIImageView!
     
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
     @IBAction func rollButtonPressed(_sender: UIButton) {
+        // Getting the url
+        let url = Bundle.main.url(forResource: "rolling dice 2", withExtension: "mp3")
+        // Make sure that we've got the url, otherwise abord
+        guard url != nil else {
+            return
+        }
+        // Create the audio player the sound
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url!)
+            audioPlayer?.play()
+        }
+        catch {
+            print("error")
+        }
         let diceArray = [#imageLiteral(resourceName: "Dice1"), #imageLiteral(resourceName: "Dice2"), #imageLiteral(resourceName: "Dice3"), #imageLiteral(resourceName: "Dice4"), #imageLiteral(resourceName: "Dice5"), #imageLiteral(resourceName: "Dice6")]
         
         diceImageViewOne.image = diceArray.randomElement()
